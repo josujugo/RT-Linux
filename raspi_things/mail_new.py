45527d95570c36299850898f7ae36b83a74d0cec
josujugo <47331920+josujugo@users.noreply.github.com>
2025-07-16 10:16:06 +0200

    code examples for raspi

---
+++ raspi_things/mail_new.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+import socket
+import smtplib
+import os
+import sys
+
+# or uncomment the following lines and set secret values here
+sender_address = 'josujugo.test@gmail.com'
+sender_password = 'thisis_test11'
+sender_server = 'smtp.gmail.com'
+sender_port = 587
+recipient_address = "josujugo@gmail.com"
+
+def get_device_ip_address():
+
+    try: 
+        if os.name == "nt":
+            # On Windows
+            result = "Running on Windows"
+            hostname = socket.gethostname()
+            result += "\nHostname:  " + hostname
+            host = socket.gethostbyname(hostname)
+            result += "\nHost-IP-Address:" + host
+            return result
+
+        elif os.name == "posix":
+            gw = os.popen("ip addr|awk '/inet / {sub(/\/.*/ "",$2);print $2}'").read().split()
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            ipaddr=''
+            for ii in gw:
+                ipaddr=ipaddr+ii+'\n'
+            
+            #gateway = gw[2]
+            host = socket.gethostname()
+            #result = "OS:\t\tRaspbian\nIP:\t\t" + ipaddr + "\nGateway:\t" + gateway + "\nHost:\t\t" + host
+            result = "OS:\t\tRaspbian\nIP:\t\t" + ipaddr + "\nHost:\t\t" + host
+            return result
+        
+        else:
+            result = os.name + " not supported yet."
+            return result
+    except:
+        return "Could not detect ip address"
+
+def send_email(text):
+    try:
+        message = "From: " + sender_address + "\nTo: " + recipient_address + "\nSubject: Device Information\n\n" + text 
+
+        server = smtplib.SMTP(sender_server, sender_port)
+        server.ehlo()
+        server.set_debuglevel(True)
+        server.starttls()
+        server.login(sender_address, sender_password)
+        server.sendmail(sender_address, recipient_address, message)
+        server.close()
+        print("Message sent:\n", message)
+
+    except:
+        print("failed to send email")
+
+message = get_device_ip_address()
+print(message)
+print("Sending email, can take a while.")
+send_email(message)
+print("Done.")
+
+sys.exit()
